64efd1a7f3879055dbbfe0ca53223f42019880b2
William Overbeck <williamoverbeck125@gmail.com>
2023-04-30 00:41:13 +0200

    feature -> start hud editing

--- modules/classes/hud.py
+++ modules/classes/hud.py
@@ -1,8 +1,15 @@
 """Methods related to editing a hud"""
 import os
+import threading
+import time
+import psutil
+import subprocess
 from modules.classes.hud_descriptions import HudDescriptions
 from modules.classes.hud_syncer import HudSyncer
+from modules.classes.gui_hud_browser import GuiHudBrowser
 from modules.classes.game import Game
+from modules.utils.functions import load_data
+from modules.utils.constants import DEVELOPMENT_DIR
 
 
 class Hud:
@@ -10,10 +17,11 @@ class Hud:
 
     def __init__(self, game_instance) -> None:
         assert isinstance(game_instance, Game)
-        self.game_instance = game_instance
+        self.game = game_instance
         self.syncer = HudSyncer()
         self.desc = HudDescriptions()
         self.hud_dir = None
+        self.timer_game_exit = None
 
     def get_dir(self):
         """Get information"""
@@ -41,6 +49,16 @@ class Hud:
         print(f"get_file_description for: {relative_path}")
         return self.desc.get_description(relative_path)
 
+    def wait_for_game_exit_then_finish_editing(self):
+        """Used to finish editing when game closes"""
+
+        if not self.game.is_running():
+            self.finish_editing()
+        else:
+            # Schedule the function to be called again
+            self.timer_game_exit = threading.Timer(2, self.wait_for_game_exit_then_finish_editing)
+            self.timer_game_exit.start()
+
     def start_editing(self, hud_dir):
         """Perform all the actions needed to start hud editing"""
         print(f"start_hud_editing: todo... ({hud_dir})")
@@ -56,19 +74,41 @@ class Hud:
         # unsync previous hud
         self.syncer.un_sync()
 
+        # Stop checking for game exit
+        if self.timer_game_exit is not None:
+            self.timer_game_exit.cancel()
+
         # enable dev mode
-        self.game_instance.activate_mode("dev")
+        self.game.activate_mode("dev")
 
         # sync the hud to the game folder
         # self.syncer.sync(
-        #     self.hud_dir, self.game_instance.get_dir("dev"), os.path.basename(self.game_instance.get_main_dir("dev"))
+        #     self.hud_dir, self.game.get_dir("dev"), os.path.basename(self.game_instance.get_main_dir("dev"))
         # )
 
         # run the game
-        # self.game_instance.run("dev")
+        self.game.run("dev")
 
         # refresh hud incase game has not restarted
+        self.game.command.execute("reload_all")
+
+        # Start checking for game exit
+        self.wait_for_game_exit_then_finish_editing()
 
     def finish_editing(self):
         """Perform all the actions needed to finish hud editing"""
         print("finish_hud_editing: todo...")
+
+
+def debug_hud():
+    """Debug the hud class"""
+    print("debug_hud")
+
+    persistent_data = load_data()
+    game_instance = Game(persistent_data)
+    huds_debug_dir = os.path.join(DEVELOPMENT_DIR, "debug", "hud_debug")
+    hud_debug_dir = os.path.join(huds_debug_dir, "Workspace", "2020HUD")
+    hud_edit = Hud(game_instance)
+    hud_edit.start_editing(hud_debug_dir)
+    # gui_browser = GuiHudBrowser(hud_edit)
+    # gui_browser.root.mainloop()
